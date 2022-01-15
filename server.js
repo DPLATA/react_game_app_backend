0bc04982730323749d476a601569d1bd31d25d9e
Diego Plata <diegogplata@gmail.com>
2022-01-15 10:58:47 -0600

    update TO-DOs in server file

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const express = require('express')
 const appConfig = require('./configs/app')
 const server = express()
-
-//TODO: remove warnings
 //TODO: setup db
 //TODO: refactor routing
+//TODO: work with data
 
 server.listen(appConfig.express_port, () => {
     let message = '(http://localhost:' + appConfig.express_port + ')'
